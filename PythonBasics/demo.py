9647489cc19e899f73afd1fa80338eb903271fe6
Monika <tmonika2006@gmail.com>
2023-06-20 17:22:03 +0100

    next day change by local

--- PythonBasics/demo.py
+++ PythonBasics/demo.py
@@ -1,9 +1,9 @@
-print('hello')
+print('hello stand up')
 
 a = 3
 print(a)
 
-b, c, d = 5, 6.3, "Good"
+b, c, d = 5, 6.3, "Good Job"
 
 print(b, c, d)
 
@@ -12,3 +12,6 @@ print("value of b is",b)
 print("{} {}".format("value is", b)) #concatenate two different data types
 
 print(type(c))
+
+print("Have a nice day")
+    
